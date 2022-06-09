e1eaac49c487f1b900c3046e58fba8ecc8433a5c
Nubi Toad <nubitoad@gmail.com>
2022-06-08 18:09:33 -0700

    Build for WebGL.

--- Assets/StandardShaderReplacer.cs
+++ Assets/StandardShaderReplacer.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 [ExecuteInEditMode]
 public class StandardShaderReplacer : MonoBehaviour
 {
+    public Shader[] allowedShaders;
     public Shader pbrDefaultShader;
 
     void OnGUI()
     {
         Assert.IsNotNull(pbrDefaultShader);
+        Assert.IsTrue(allowedShaders.Length != 0);
         foreach (var renderer in (Renderer[]) Object.FindObjectsOfType(typeof(Renderer)))
         {
             foreach (var mat in renderer.sharedMaterials) {
-                if (mat.shader.name == "Standard")
+                if (!allowedShaders.Contains(mat.shader))
                 {
                     Debug.LogWarning("Replacing shader on '" + renderer.gameObject.name + "'...");
                     mat.shader = pbrDefaultShader;
